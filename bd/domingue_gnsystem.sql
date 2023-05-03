e931180aaf604443dbbfe5641aa7c5261304fcac
laelramos <laelrf@gmail.com>
2023-05-03 15:46:37 -0300

    bd

---
+++ bd/domingue_gnsystem.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 03-Maio-2023 às 15:38
+-- Versão do servidor: 8.0.32-0ubuntu0.22.04.2
+-- versão do PHP: 8.1.2-1ubuntu2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `domingue_gnsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_categorie` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dataUpload` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `categories`
+--
+
+INSERT INTO `categories` (`id_categorie`, `name`, `dataUpload`) VALUES
+(1, 'Funko Pop', '2023-05-03'),
+(2, 'Figure', '2023-05-03'),
+(3, 'Q Posket', '2023-05-03');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `images`
+--
+
+CREATE TABLE `images` (
+  `id_image` int NOT NULL,
+  `id_category` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dataUpload` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `products`
+--
+
+CREATE TABLE `products` (
+  `id` int UNSIGNED NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ean` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `franchise` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `manufacturer` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_categorie` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `products`
+--
+
+INSERT INTO `products` (`id`, `description`, `ean`, `category`, `franchise`, `manufacturer`, `id_categorie`) VALUES
+(6, 'Funko John Wick 580', '889698472388', '', NULL, NULL, 1),
+(5, 'Funko Ronan 75', '849803051761', NULL, NULL, NULL, NULL),
+(7, 'Funko Itachi 578', '889698404532', NULL, NULL, NULL, NULL),
+(18, 'Funko azog 48', '849803037413', NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `product_entries`
+--
+
+CREATE TABLE `product_entries` (
+  `id` bigint UNSIGNED NOT NULL,
+  `product_id` int DEFAULT NULL,
+  `entry_quantity` int DEFAULT NULL,
+  `entry_date` date DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `batch_number` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `product_entries`
+--
+
+INSERT INTO `product_entries` (`id`, `product_id`, `entry_quantity`, `entry_date`, `description`, `batch_number`) VALUES
+(1, 48, 10, '2023-04-30', 'Random description', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `email`, `level`, `password`) VALUES
+(1, 'laelramos', 'laelrf@gmail.com', 'administrador', 'eda5786e8c180c0b347d1c288ff8c1ff504ec363856b5b9a55424f1e16e07e78'),
+(6, 'renatogomes', 'renatokakashi0109@gmail.com', 'administrador', 'd2413bec8eacaa99151cdffeb86858f793c2ba3c827c4fa10a2224dfad9b107d'),
+(404, 'admin', 'admin@admin.com', 'dev', '8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Índices para tabela `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id_image`);
+
+--
+-- Índices para tabela `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Índices para tabela `product_entries`
+--
+ALTER TABLE `product_entries`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categorie` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `images`
+--
+ALTER TABLE `images`
+  MODIFY `id_image` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT de tabela `product_entries`
+--
+ALTER TABLE `product_entries`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=405;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
